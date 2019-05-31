a3d14385e31cfde5bb1fe811261eab9b9123d56d
I3lacKAsH <50867727+I3lacKAsH@users.noreply.github.com>
2019-05-31 11:23:39 +0700

    Wipcargo

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -4,27 +4,63 @@
 
 package XI.Project;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
     public static void main(String[] args) {
-        int age = 18;
-        String name = "Wippo";
-        double money = 1100.11;
-        
-        boolean online = true;
-        String status;
-        
-        System.out.println("Hi! my name is " + name + " i'm " + age + " Y/O ");
-        
-        if(online == true){
-            status = "Online";
-        } else {
-            status = "Offline";
+        int item =4;
+        int limit = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("มีไอเทม : " + item);
+        System.out.println("ค่าสูงสุดของโกดังในการเก็บไอเท็ม : " + limit);
+
+        System.out.println("หากต้องการ\nเก็บไอเท็ม กด 1\nนำไอเท็มออก กด 2\nเช็คไอเท็ม กด 3");
+
+        System.out.print("โปรดใส่เลข : ");
+        int x = sc.nextInt();
+
+        if (x == 1) {
+            if (item < limit) {
+                System.out.print("ทำการเพิ่มไอเท็ม : ");
+                int amount1 = sc.nextInt();
+                if (item + amount1 > limit) {
+                    System.out.println("ไอเท็มเกินลิมิต");
+                } else {
+                    System.out.println("จำนวนไอเทมรวม : " + (item + amount1));
+                }
+
+            } else {
+                System.out.println("ไอเท็มเต็มแล้ว");
+
+            }
+            System.out.println("===============================================");
         }
+
+        else if (x == 2) {
+            if (item == 0) {
+                System.out.println("ไม่มีไอเท็มในโกดัง");
+            } else {
+                System.out.print("นำไอเท็มออก : ");
+                int amount2 = sc.nextInt();
+                if (item < amount2) {
+                    System.out.println("ไอเท็มไม่เพียงพอ");
+                } else {
+                    System.out.println("เหลือไอเท็มในโกดัง : " + (item - amount2));
+                }
+
+            }
+        }
+
+
+        else if (x == 3) {
+            System.out.println("ทำการเช็คไอเท็ม");
+            System.out.println("มีไอเทม : " + item);
+        } 
+        else {
+            System.out.println("ไม่มีคำสั่งดังกล่าว");
         
-        System.out.println("I'm currently " + status + " and have " + money + "THB. left.");
-    }
-}
+    
